75fd366ec6f5ef95f1f78889b72adaa9d3eb0df0
Scott Fitsimones <scott@Scotts-MacBook-Pro.local>
2021-11-24 11:16:13 -0600

    citydao museum

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import NFT from './NFT.js';
+import { NftGallery } from 'react-nft-gallery';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h2>CityDAO Museum</h2>
-        <NFT 
+        <NftGallery ownerAddress="daocity.eth" darkMode={true} />
+        {/* <NFT 
         collection={"0x8d04a8c79ceb0889bdd12acdf3fa9d207ed3ff63"}
         item={"501"}
         curator={"casey.eth"}
@@ -15,7 +17,7 @@ function App() {
         collection={"0x448f3219cf2a23b0527a7a0158e7264b87f635db"}
         item={"758"}
         curator={""}
-        description={""}/>
+        description={""}/> */}
       </header>
     </div>
   );
