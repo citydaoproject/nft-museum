39a0a25dfbb1231053905886fb2b8f417c4a6d41
Tuncay Kaplan <trkaplan@gmail.com>
2022-03-28 04:43:32 +0300

    feat: auto retry on API request fail

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import './App.css';
-import NFT from './NFT.js';
-import { NftGallery } from 'react-nft-gallery';
+import "./App.css";
+import NFT from "./NFT.js";
+import { NftGallery } from "react-nft-gallery";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h2>Art Musem of CityDAO</h2>
-        <p>Curated by CityDAO citizen <a href="https://twitter.com/getprsm">Casey</a>.  <br/><span className="Donate"> Donations accepted to daocity.eth</span></p>
+        <p>
+          Curated by CityDAO citizen{" "}
+          <a href="https://twitter.com/getprsm">Casey</a>. <br />
+          <span className="Donate"> Donations accepted to daocity.eth</span>
+        </p>
         <NftGallery
           ownerAddress="daocity.eth"
           darkMode={true}
           isProxyApi={true}
           apiUrl={process.env.REACT_APP_OPENSEA_API_URL}
+          autoRetry={true}
         />
         {/* <NFT 
         collection={"0x8d04a8c79ceb0889bdd12acdf3fa9d207ed3ff63"}
